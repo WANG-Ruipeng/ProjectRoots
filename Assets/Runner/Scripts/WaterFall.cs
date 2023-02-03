7690efee2c01651621d96554e3d2ecfdf177b2c9
WANG-Ruipeng <783132025@qq.com>
2023-02-04 01:17:20 +0800

    完成水流prefab

---
+++ Assets/Runner/Scripts/WaterFall.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using HyperCasual.Core;
+using UnityEngine;
+
+namespace HyperCasual.Runner
+{
+    /// <summary>
+    /// Ends the game on collision, forcing a lose state.
+    /// </summary>
+
+    public class WaterFall : Spawnable
+    {
+        //TODO:策划说要让玩家碰到瀑布就停止
+    }
+
+}
